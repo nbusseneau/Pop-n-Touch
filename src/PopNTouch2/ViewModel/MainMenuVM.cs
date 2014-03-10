ef6d15541ab69aedea7543c62ad1197d5e79a289
Gwenegan Hudin <gwenegan.hudin@insa-rennes.fr>
2014-03-10 12:04:27 +0100

    Command relaying sweet, sweet parameters
    http://msdn.microsoft.com/en-us/magazine/dn237302.aspx

--- src/PopNTouch2/ViewModel/MainMenuVM.cs
+++ src/PopNTouch2/ViewModel/MainMenuVM.cs
@@ -4,11 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using PopNTouch2.Model;
 
 namespace PopNTouch2.ViewModel
 {
     public class MainMenuVM : ViewModelBase
     {
+        // FIXME : Temporary, will be a Song in the future
+        private string selectedSong;
+        public string SelectedSong 
+        { 
+            get 
+            {
+                return this.selectedSong;
+            } 
+        }
+
+        ICommand selectSong;
+        public ICommand SelectSong
+        {
+            get
+            {
+                if (this.selectSong == null)
+                    this.selectSong = new RelayCommand<string>( songId =>
+                        {
+                            // TODO : Convert from string songId to Song
+                            this.selectedSong = songId;
+                            RaisePropertyChanged("SelectedSong");
+                        }
+                    );
+
+                return this.selectSong;
+            }
+        }
     }
 
 }
--- src/PopNTouch2/ViewModel/RelayCommand.cs
+++ src/PopNTouch2/ViewModel/RelayCommand.cs
@@ -28,4 +28,29 @@ namespace PopNTouch2.ViewModel
         }
 
     }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private Action<T> execute;
+
+        public RelayCommand(Action<T> execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return parameter is T;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        [System.Diagnostics.DebuggerStepThrough]
+        public void Execute(object parameter)
+        {
+            if (execute != null)
+                execute((T)parameter);
+        }
+
+    }
 }
